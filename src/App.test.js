6e59fd3c4cc9ac67065c993e91b14ebefb397c56
Guilherme Emerick <emerick.perth@gmail.com>
2026-03-09 19:16:46 -0300

    test: update App test to match new branding and add i18n import

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,9 @@
 import { render, screen } from '@testing-library/react';
+<<<<<<< HEAD
 import App from './App.js';
+=======
+import App from './App';
+>>>>>>> e5f0023 (test: update App test to match new branding and add i18n import)
 import './i18n/i18n';
 
 test('renders the app without crashing', () => {
